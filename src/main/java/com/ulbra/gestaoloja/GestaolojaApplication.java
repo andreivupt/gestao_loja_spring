989057c0714466e177b98bb5273a46d9e1de3cb6
andrei.vupt <andrei.vupt@gmail.com>
2017-11-22 14:15:53 -0200

    iniciando projeto com spring

---
+++ src/main/java/com/ulbra/gestaoloja/GestaolojaApplication.java
@@ -0,0 +1,12 @@
+package com.ulbra.gestaoloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaolojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaolojaApplication.class, args);
+	}
+}
